39a561fe7bad6fe7d0cbe52dbaaac0b41f95cf29
DaShork <nhatminhvo2311@gmail.com>
2026-04-21 15:55:44 +0700

    feat(script + api): change APIprovider from Openweather to WeatherAPI, added new README.MD

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    API_KEY: "c8ef8b90ff1042fa9ef65952262104",
+    API_PROVIDER: "WeatherAPI.com",
+    API_BASE_URL: "https://api.weatherapi.com/v1"
+};
+
+// EXPORT THEO DANG MODULE DE CAC FILE KHAC CO THE SU DUNG
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
--- script.js
+++ script.js
@@ -23,104 +23,93 @@ const
 
 // GOI API
 const
-    // API LAY TTHONG TIN CHUNG
-    WEATHER_API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=a5bb4718b30b6f58f58697997567fffa&q=`,
-    // API LAY THONG TIN CHI TIET
-    WEATHER_DATA_ENDPOINT = `https://api.openweathermap.org/data/2.5/onecall?appid=a5bb4718b30b6f58f58697997567fffa&exclude=minutely&units=metric&`,
+    // API LAY THONG TIN HIEN TAI VA TUAN SAU
+    WEATHER_API_ENDPOINT = `https://api.weatherapi.com/v1/current.json?key=${CONFIG.API_KEY}&q=`,
+    // API LAY THONG TIN DU BAO 7 NGAY
+    WEATHER_FORECAST_ENDPOINT = `https://api.weatherapi.com/v1/forecast.json?key=${CONFIG.API_KEY}&q=`,
     // API LAY THONG TIN ZIP/POST CODE
-    WEATHER_ZIP_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=a5bb4718b30b6f58f58697997567fffa&zip=`;
+    WEATHER_ZIP_ENDPOINT = `https://api.weatherapi.com/v1/current.json?key=${CONFIG.API_KEY}&q=`;
 
-// KHAI BAO KIEU DU LIEU THOI GIAN
-const
-    options1 = {
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-    },
-    options = {
+// HAM TIM VI TRI NGUOI DUNG VA TRA VE THONG TIN THHOI TIET
+function findLocation() {
+    Forecast.innerHTML = "";
+    const location = userLocation.value;
+    
+    // GOI API DU BAO 7 NGAY + THONG TIN HIEN TAI
+    fetch(WEATHER_FORECAST_ENDPOINT + location + "&days=7&aqi=yes").then((res) => res.json()).then(data => {
+        if (!data.current) {
+            alert("Location not found. Please try again.");
+            return
+        }
+
+        console.log(data);
+        getWeatherData(data);
+    }).catch(error => {
+        alert("Error fetching weather data: " + error.message);
+    });
+}
+
+// HAM XU LY THONG TIN VA TRA VE KET QUA
+function getWeatherData(data) {
+    // THAY DOI TEN TP VA ICON NHIET DO - WeatherAPI structure
+    const location = data.location;
+    const current = data.current;
+    const forecast = data.forecast.forecastday;
+    
+    city.innerHTML = location.name + ", " + location.country;
+    // LAY ICON TU THU VIEN CUA WEATER API
+    weatherIcon.style.background = `url(https:${current.condition.icon})`;
+    
+    // LAY DATA TU API VA IN RA KHUNG WEATHER CHINH
+    console.log(data);
+    temperature.innerHTML = TempConvert(current.temp_c);
+    feelsLike.innerHTML = "Feels like " + Math.round(current.feelslike_c) + "°C";
+    description.innerHTML = `<i class = "fa-brands fa-cloudversify"></i> &nbsp;` + current.condition.text;
+    
+    // DINH DANG KIEU DU LIEU NGAY THANG
+    const localDate = new Date(current.last_updated);
+    date.innerHTML = localDate.toLocaleString("en-US", {
         weekday: "long",
         month: "long",
         day: "numeric",
         hour: "numeric",
         minute: "numeric",
         hour12: true,
-    };
-
-// HAM TIM VI TRI NGUOI DUNG VA TRA VE THONG TIN THHOI TIET
-function findLocation() {
-    Forecast.innerHTML = "";
-    // ID ELSE DE XU LY TRUONG HOP NGUOI DUNG NHAP VAO ZIP CODE POST CODE HOAC NHAP VAO TEN THANH PHO
-    if (typeof userLocation.value === 'number') {
-        // GOI API XU LY ZIP CODE
-        fetch(WEATHER_ZIP_ENDPOINT + userLocation.value).then((res) => res.json()).then(data => {
-            if (data.cod != "" && data.cod != 200) {
-                alert(data.message);
-                return
-            }
-
-            console.log(data);
-            getWeatherData(data);
-        });
-    } else {
-        // GOI API XU LY TEN THANH PHO
-        fetch(WEATHER_API_ENDPOINT + userLocation.value).then((res) => res.json()).then(data => {
-            if (data.cod != "" && data.cod != 200) {
-                alert(data.message);
-                return
-            }
-            console.log(data);
-
-            getWeatherData(data);
-        });
-    }
-}
-
-// HAM XU LY THOONG TIN VA TRA VE KET QUA
-function getWeatherData(data) {
-    // THAY DOI TEN TP VA ICON NHIET DO
-    city.innerHTML = data.name + ", " + data.sys.country;
-    weatherIcon.style.background = `url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`;
-    // LAY LATIYUTE VA LONGTITUTE TU DU LIEU CUA API ZIP HOAC API CHUNG
-    fetch(`${WEATHER_DATA_ENDPOINT}&lon=${data.coord.lon}&lat=${data.coord.lat}`).then((res) => res.json()).then(data => {
-        // LAY DATA TU API VA IN RA KHUNG WEATHER CHINH
-        console.log(data);
-        temperature.innerHTML = TempConvert(data.current.temp);
-        feelsLike.innerHTML = "Feels like " + data.current.feels_like;
-        description.innerHTML = `<i class = "fa-brands fa-cloudversify"></i> &nbsp;` + data.current.weather[0].description;
-        date.innerHTML = getLongFormatDateTime(data.current.dt, data.timezone_offset, options);
-        // LAY DATA TU API VA IN RA THONG TIN LIEN QUAN
-        HValue.innerHTML = Math.round(data.current.humidity) + "<span>%</span>";
-        WValue.innerHTML = Math.round(data.current.wind_speed) + "<span>m/s</span>";
-        SRValue.innerHTML = getLongFormatDateTime(data.current.sunrise, data.timezone_offset, options1);
-        SSValue.innerHTML = getLongFormatDateTime(data.current.sunset, data.timezone_offset, options1);
-        CValue.innerHTML = data.current.clouds + "<span>%</span>";
-        UVValue.innerHTML = data.current.uvi;
-        PValue.innerHTML = data.current.pressure + "<span>hPa</span>";
-        // LAY DATA TU API VA TAO CAC O DATA CAC NGAY TRONG TUAN
-        data.daily.forEach((weather) => {
-            let div = document.createElement("div");
-            let daily = getLongFormatDateTime(weather.dt, 0, options).split(" at ");
-            div.innerHTML = daily[0]
-            div.innerHTML += `<img src = "https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png"/>`;
-            div.innerHTML += `<p class = "forecast-desc">${weather.weather[0].description}</p>`;
-            div.innerHTML += `<span>Min: <span>${TempConvert(weather.temp.min)}</span>&nbsp;Max: <span>${TempConvert(weather.temp.max)}</span></span>`;
-            Forecast.append(div);
-        });
+    });
+    
+    // LAY DATA TU API VA IN RA THONG TIN LIEN QUAN
+    HValue.innerHTML = Math.round(current.humidity) + "<span>%</span>";
+    WValue.innerHTML = Math.round(current.wind_kph / 3.6) + "<span>m/s</span>"; // Convert kph to m/s
+    
+    // LAY DATA THOI GIAN BINH MINH VA HOANG HON
+    const forecastToday = forecast[0];
+    SRValue.innerHTML = "<span>Sunrise:</span>" +forecastToday.astro.sunrise;
+    SSValue.innerHTML = "<span>Sunset:</span>" + forecastToday.astro.sunset;
+    
+    CValue.innerHTML = current.cloud + "<span>%</span>";
+    UVValue.innerHTML = Math.round(current.uv * 10) / 10;
+    PValue.innerHTML = Math.round(current.pressure_mb) + "<span>hPa</span>";
+    
+    // LAY DATA TU API VA TAO CAC O DATA CAC NGAY TRONG TUAN
+    forecast.forEach((day) => {
+        let div = document.createElement("div");
+        const dayDate = new Date(day.date);
+        const dayName = dayDate.toLocaleString("en-US", { weekday: "long", month: "short", day: "numeric" });
+        
+        div.innerHTML = dayName;
+        div.innerHTML += `<img src = "https:${day.day.condition.icon}"/>`;
+        div.innerHTML += `<p class = "forecast-desc">${day.day.condition.text}</p>`;
+        div.innerHTML += `<span>Min: <span>${TempConvert(day.day.mintemp_c)}</span>&nbsp;Max: <span>${TempConvert(day.day.maxtemp_c)}</span></span>`;
+        Forecast.append(div);
     });
 }
+
 // TU DONG RELOAD THONG TIN SAU KHI THAY DOI LOAI NHIET DO
 converter.addEventListener("change", () => {
     if (userLocation.value) {
         findLocation();
     }
 });
-
-// XU LY THONG TIN THOI GIAN
-function getLongFormatDateTime(dtValue, offSet, options) {
-    const unixTime = dtValue + offSet;
-    const date = new Date(unixTime * 1000);
-    return date.toLocaleString("en-US", { ...options, timeZone: 'UTC' });
-}
 
 // XU LY THONG TIN KHI DOI TU DO C -> F
 function TempConvert(temp) {
